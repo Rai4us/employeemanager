c6eb7a519df240fe33617ce8af6c45cff92ad552
Yevhenii Shevchenko <yshevchenko@atlassian.com>
2022-12-04 22:48:53 +1030

    Adding Retrieving logic

--- employeemanager_FE/employeemanagerapp/src/app/app.component.ts
+++ employeemanager_FE/employeemanagerapp/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Employee} from "./employee";
+import {EmployeeService} from "./employee.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'employeemanagerapp';
+
+  public employees: Employee[] | undefined;
+
+  constructor(private employeeService: EmployeeService) {
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (value: Employee[]) => {
+        this.employees = value;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    )
+  }
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
 }
